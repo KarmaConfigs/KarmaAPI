3500ecd14c31b974c7ba45f2aae73ffa23abb30e
Karma <karmaconfigs@gmail.com>
2021-10-17 15:19:46 +0200

    Updated sources to avoid data lost

--- Bungee/src/main/java/ml/karmaconfigs/api/bungee/makeiteasy/BarUtil.java
+++ Bungee/src/main/java/ml/karmaconfigs/api/bungee/makeiteasy/BarUtil.java
@@ -1,3 +1,5 @@
+package ml.karmaconfigs.api.bungee.makeiteasy;
+
 /*
  * Copyright 2019 Ivan Pekov (MrIvanPlays)
 
@@ -17,34 +19,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **/
-package ml.karmaconfigs.api.bungee.makeiteasy;
 
-/*
- * This file is part of KarmaAPI, licensed under the MIT License.
- *
- *  Copyright (c) karma (KarmaDev) <karmaconfigs@gmail.com>
- *  Copyright (c) contributors
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
-
-import com.google.common.base.Preconditions;
 import ml.karmaconfigs.api.common.boss.BossColor;
 import ml.karmaconfigs.api.common.boss.BossFlag;
 import ml.karmaconfigs.api.common.boss.BossType;
@@ -63,15 +38,44 @@ import java.util.*;
 /**
  * Represents a boss bar
  */
-class BarUtil {
+final class BarUtil {
 
+    /**
+     * Boss bar title
+     */
     private BaseComponent[] title;
+
+    /**
+     * Boss bar color
+     */
     private BossColor color;
+    /**
+     * Boss bar style
+     */
     private BossType style;
+    /**
+     * Boss bar flags
+     */
     private final Set<BossFlag> flags;
+
+    /**
+     * Boss bar health
+     */
     private float health;
+
+    /**
+     * Boss bar uuid
+     */
     private final UUID uuid;
+
+    /**
+     * If the boss bar is visible
+     */
     private boolean visible;
+
+    /**
+     * Players that will see the boss bar
+     */
     private final List<ProxiedPlayer> players;
 
     /**
@@ -81,17 +85,21 @@ class BarUtil {
      * @param color boss bar color
      * @param style boss bar style
      * @param health boss bar health. Should be a number between 0.1 and 1
+     * @throws IllegalStateException if the health is not a valid health value
      */
-    public BarUtil(final @NotNull BaseComponent[] title, final @NotNull BossColor color, final @NotNull BossType style, final float health) {
+    public BarUtil(final @NotNull BaseComponent[] title, final @NotNull BossColor color, final @NotNull BossType style, final float health) throws IllegalArgumentException {
         this.title = title;
         this.color = color;
         this.style = style;
-        Preconditions.checkArgument(health < 1 || health > 0, "Health must be between 0.1 and 1");
         this.health = health;
         this.flags = EnumSet.noneOf(BossFlag.class);
         this.uuid = UUID.randomUUID();
         this.players = new ArrayList<>();
         this.visible = true;
+
+        if (health > 1 || health < 0.1) {
+            throw new IllegalArgumentException("The boss health must be between 0.1 and 1. Provided: " + health);
+        }
     }
 
     /**
@@ -120,8 +128,7 @@ class BarUtil {
      * @param players the players you wish to add
      * @see #addPlayer(ProxiedPlayer)
      */
-    public final void addPlayers(final @NotNull Iterable<ProxiedPlayer> players) {
-        Preconditions.checkNotNull(players, "players");
+    void addPlayers(final @NotNull Iterable<ProxiedPlayer> players) {
         for (ProxiedPlayer player : players) {
             addPlayer(player);
         }
@@ -132,8 +139,7 @@ class BarUtil {
      *
      * @param player the player you wish to add
      */
-    public final void addPlayer(final @NotNull ProxiedPlayer player) {
-        Preconditions.checkNotNull(player, "player");
+    void addPlayer(final @NotNull ProxiedPlayer player) {
         if (!players.contains(player)) {
             players.add(player);
         }
@@ -148,7 +154,7 @@ class BarUtil {
      * @param playerArray the players you wish to remove
      * @see #removePlayer(ProxiedPlayer)
      */
-    public final void removePlayers(final @NotNull ProxiedPlayer... playerArray) {
+    void removePlayers(final @NotNull ProxiedPlayer... playerArray) {
         Iterator<ProxiedPlayer> iterator = players.iterator();
         Set<UUID> uuids = new HashSet<>();
         for (ProxiedPlayer player : playerArray)
@@ -171,7 +177,7 @@ class BarUtil {
     /**
      * Removes all added players from the boss bar
      */
-    public final void removeAllPlayers() {
+    void removeAllPlayers() {
         Iterator<ProxiedPlayer> iterator = players.iterator();
         if (iterator.hasNext()) {
             do {
@@ -192,7 +198,7 @@ class BarUtil {
      *
      * @param player the player you wish to remove
      */
-    public final void removePlayer(final @NotNull ProxiedPlayer player) {
+    void removePlayer(final @NotNull ProxiedPlayer player) {
         if (player.isConnected() && visible) {
             sendPacket(player, removePacket());
         }
@@ -204,8 +210,8 @@ class BarUtil {
      *
      * @param title the title you wish to set
      */
-    public final void setTitle(final @NotNull BaseComponent... title) {
-        this.title = Preconditions.checkNotNull(title, "title");
+    void setTitle(final @NotNull BaseComponent... title) {
+        this.title = title;
         if (visible) {
             BossBar packet = new BossBar(uuid, 3);
             packet.setTitle(ComponentSerializer.toString(title));
@@ -218,9 +224,13 @@ class BarUtil {
      * The minimum is 0.1 and the maximum is 1
      *
      * @param health the health you wish to set.
+     * @throws IllegalArgumentException if the health is not a valid health value
      */
-    public final void setHealth(final float health) {
-        Preconditions.checkArgument(health < 1 || health > 0, "Health must be between 0.1 and 1");
+    void setHealth(final float health) throws IllegalArgumentException {
+        if (health > 1 || health < 0.1) {
+            throw new IllegalArgumentException("The boss health must be between 0.1 and 1. Provided: " + health);
+        }
+
         this.health = health;
         if (visible) {
             BossBar packet = new BossBar(
@@ -235,7 +245,7 @@ class BarUtil {
      *
      * @param newColor the color you wish to set
      */
-    public final void setColor(final @NotNull BossColor newColor) {
+    void setColor(final @NotNull BossColor newColor) {
         if (visible) {
             color = newColor;
             setDivisions(color, style);
@@ -247,7 +257,7 @@ class BarUtil {
      *
      * @param newStyle the style you wish to set
      */
-    public final void setStyle(final @NotNull BossType newStyle) {
+    void setStyle(final @NotNull BossType newStyle) {
         if (visible) {
             style = newStyle;
             setDivisions(color, style);
@@ -259,7 +269,7 @@ class BarUtil {
      *
      * @param visible value
      */
-    public final void setVisible(final boolean visible) {
+    void setVisible(final boolean visible) {
         boolean previous = this.visible;
         if (previous && !visible) {
             sendToAffected(removePacket());
@@ -274,7 +284,7 @@ class BarUtil {
      *
      * @param flags the flags you wish to add
      */
-    public final void addFlags(final @NotNull BossFlag... flags) {
+    void addFlags(final @NotNull BossFlag... flags) {
         if (this.flags.addAll(Arrays.asList(flags)) && visible) {
             sendToAffected(updateFlags());
         }
@@ -285,7 +295,7 @@ class BarUtil {
      *
      * @param flag the flag you wish to remove
      */
-    public final void removeFlag(final @NotNull BossFlag flag) {
+    void removeFlag(final @NotNull BossFlag flag) {
         if (this.flags.remove(flag) && visible) {
             sendToAffected(updateFlags());
         }
@@ -296,7 +306,7 @@ class BarUtil {
      *
      * @param flags the flag(s) you wish to remove
      */
-    public final void removeFlags(final @NotNull BossFlag... flags) {
+    void removeFlags(final @NotNull BossFlag... flags) {
         removeFlags(Arrays.asList(flags));
     }
 
@@ -305,7 +315,7 @@ class BarUtil {
      *
      * @param flags the flags you wish to remove
      */
-    public final void removeFlags(final @NotNull Collection<BossFlag> flags) {
+    void removeFlags(final @NotNull Collection<BossFlag> flags) {
         if (this.flags.removeAll(flags) && visible) {
             sendToAffected(updateFlags());
         }
@@ -316,7 +326,7 @@ class BarUtil {
      *
      * @return players, if no one added empty list
      */
-    public final Collection<ProxiedPlayer> getPlayers() {
+    Collection<ProxiedPlayer> getPlayers() {
         return Collections.unmodifiableList(players);
     }
 
@@ -325,7 +335,7 @@ class BarUtil {
      *
      * @return title
      */
-    public final BaseComponent[] getTitle() {
+    BaseComponent[] getTitle() {
         return title;
     }
 
@@ -334,7 +344,7 @@ class BarUtil {
      *
      * @return health
      */
-    public final float getHealth() {
+    float getHealth() {
         return health;
     }
 
@@ -343,7 +353,7 @@ class BarUtil {
      *
      * @return color
      */
-    public final BossColor getColor() {
+    BossColor getColor() {
         return color;
     }
 
@@ -352,7 +362,7 @@ class BarUtil {
      *
      * @return style
      */
-    public final BossType getStyle() {
+    BossType getStyle() {
         return style;
     }
 
@@ -362,7 +372,7 @@ class BarUtil {
      * @return <code>true</code> if visible, otherwise
      * <code>false</code>
      */
-    public final boolean isVisible() {
+    boolean isVisible() {
         return visible;
     }
 
@@ -371,12 +381,17 @@ class BarUtil {
      *
      * @return flags, if no one added empty set
      */
-    public final Collection<BossFlag> getFlags() {
+    Collection<BossFlag> getFlags() {
         return Collections.unmodifiableSet(flags);
     }
 
+    /**
+     * Get the boss bar as string
+     * 
+     * @return boss bar util as string
+     */
     @Override
-    public final String toString() {
+    public String toString() {
         StringBuilder titleBuilder = new StringBuilder();
         for (BaseComponent component : title) {
             titleBuilder.append(component.toString()).append(", ");
@@ -406,16 +421,46 @@ class BarUtil {
      */
     public static final class Builder {
 
+        /**
+         * The boss bar title
+         */
         private BaseComponent[] title;
+
+        /**
+         * The boss bar color
+         */
         private BossColor color;
+        /**
+         * The boss bar style
+         */
         private BossType style;
-        private Set<BossFlag> flags;
+
+        /**
+         * The boss bar flags
+         */
+        private final Set<BossFlag> flags;
+
+        /**
+         * The boss bar health
+         */
         private float health;
-        private List<ProxiedPlayer> players;
+
+
+        /**
+         * Players that will see the boss bar
+         */
+        private final List<ProxiedPlayer> players;
+        
+        /**
+         * If the boss bar is visible
+         */
         private boolean visible;
 
+        /**
+         * Initialize the boss bar
+         */
         public Builder() {
-            this.title = new ComponentBuilder("Title not specified").color(ChatColor.YELLOW).create();
+            this.title = new ComponentBuilder("KarmaAPI boss bar").color(ChatColor.AQUA).create();
             this.color = BossColor.PINK;
             this.style = BossType.SOLID;
             this.flags = EnumSet.noneOf(BossFlag.class);
@@ -424,41 +469,88 @@ class BarUtil {
             this.visible = true;
         }
 
+        /**
+         * Set the boss bar title
+         * 
+         * @param title the boss bar title
+         * @return this instance
+         */
         public Builder title(final @NotNull BaseComponent... title) {
             this.title = title;
             return this;
         }
 
+        /**
+         * Add a player to the boss bar
+         * 
+         * @param playerArray the players to add
+         * @return this instance
+         */
         public Builder player(final @NotNull ProxiedPlayer... playerArray) {
             players.addAll(Arrays.asList(playerArray));
             return this;
         }
 
+        /**
+         * Set the boss bar health
+         * 
+         * @param health the boss bar health
+         * @return this instance
+         */
         public Builder health(final float health) {
             this.health = health;
             return this;
         }
 
+        /**
+         * Set the boss bar flags
+         * 
+         * @param flags the boss bar flags
+         * @return this instance
+         */
         public Builder flags(final @NotNull BossFlag... flags) {
             this.flags.addAll(Arrays.asList(flags));
             return this;
         }
 
+        /**
+         * Set the boss bar color
+         * 
+         * @param color the boss bar color
+         * @return this instance
+         */
         public Builder color(final @NotNull BossColor color) {
             this.color = color;
             return this;
         }
 
+        /**
+         * Set the boss bar style
+         * 
+         * @param style the boss bar style
+         * @return this instance
+         */
         public Builder style(final @NotNull BossType style) {
             this.style = style;
             return this;
         }
 
+        /**
+         * Set if the boss bar is visible
+         * 
+         * @param visible if the boss bar is visible
+         * @return this instance
+         */
         public Builder visible(final boolean visible) {
             this.visible = visible;
             return this;
         }
 
+        /**
+         * Build the boss bar
+         * 
+         * @return a new bar util instance
+         */
         public BarUtil build() {
             BarUtil bossBar = new BarUtil(title, color, style, health);
             bossBar.addFlags(flags.toArray(new BossFlag[0]));
@@ -468,8 +560,11 @@ class BarUtil {
         }
     }
 
-    //
-
+    /**
+     * Serialize the boss bar flags
+     * 
+     * @return the serialized flags
+     */
     private byte serializeFlags() {
         byte flagMask = 0x0;
         if (flags.contains(BossFlag.DARKEN_SCREEN)) {
@@ -484,6 +579,12 @@ class BarUtil {
         return flagMask;
     }
 
+    /**
+     * Set boss bar division
+     * 
+     * @param color the boss bar color
+     * @param style the boss bar style
+     */
     private void setDivisions(final @NotNull BossColor color, final @NotNull BossType style) {
         BossBar packet = new BossBar(uuid, 4);
         packet.setColor(color.ordinal());
@@ -491,6 +592,11 @@ class BarUtil {
         sendToAffected(packet);
     }
 
+    /**
+     * Update the boss bar flags
+     * 
+     * @return this instance
+     */
     private BossBar updateFlags() {
         BossBar packet = new BossBar(
                 uuid, 5);
@@ -498,6 +604,11 @@ class BarUtil {
         return packet;
     }
 
+    /**
+     * Get the boss bar add packet
+     * 
+     * @return the boss bar add packet
+     */
     private BossBar addPacket() {
         BossBar packet = new BossBar(
                 uuid, 0);
@@ -509,6 +620,11 @@ class BarUtil {
         return packet;
     }
 
+    /**
+     * Send the boss bar to the specified players
+     * 
+     * @param packet the boss bar to send
+     */
     private void sendToAffected(final @NotNull DefinedPacket packet) {
         for (ProxiedPlayer player : players) {
             if (player.isConnected() && visible) {
@@ -517,12 +633,23 @@ class BarUtil {
         }
     }
 
+    /**
+     * Send a packet to the player
+     * 
+     * @param player the player 
+     * @param packet the packet
+     */
     private void sendPacket(final @NotNull ProxiedPlayer player, final @NotNull DefinedPacket packet) {
         if (player.getPendingConnection().getVersion() >= ProtocolConstants.MINECRAFT_1_9) {
             player.unsafe().sendPacket(packet);
         }
     }
 
+    /**
+     * Remove the boss bar
+     * 
+     * @return a clean boss bar
+     */
     private BossBar removePacket() {
         return new BossBar(uuid, 1);
     }
